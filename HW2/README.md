8a34be1ec714ffc32cfb63f5582ee111ab56da74
paolovelove <pao.thongmotai@gmail.com>
2018-09-13 12:21:56 -0500

    Final

---
+++ HW2/README.md
@@ -0,0 +1,4 @@
+#To run the program for DFGS and IDTS
+	run main.py
+#To run the program for A*
+	run a-star-python.py
--- a-star-python.py
+++ a-star-python.py
@@ -3,45 +3,55 @@
 import sys
 import datetime
 
+# Generate All posible moves for the input state
 def moves(current_state): 
-    output = []  
+    output = []
     puzzle = eval(current_state)
     i = 0
+    
+    # Looking for zero in row
     while 0 not in puzzle[i]:
         i += 1
-    j = puzzle[i].index(0); #looking for 0
+    j = puzzle[i].index(0); # Looking for zero in column
 
     if i > 0:                                   
-        puzzle[i][j], puzzle[i-1][j] = puzzle[i-1][j], puzzle[i][j];  #move up
+        puzzle[i][j], puzzle[i-1][j] = puzzle[i-1][j], puzzle[i][j];  # move up
+        # Add the result to output list
         output.append(str(puzzle))
         puzzle[i][j], puzzle[i-1][j] = puzzle[i-1][j], puzzle[i][j]; 
       
     if i < 3:                                   
-        puzzle[i][j], puzzle[i+1][j] = puzzle[i+1][j], puzzle[i][j]   #move down
+        puzzle[i][j], puzzle[i+1][j] = puzzle[i+1][j], puzzle[i][j]   # move down
+        # Add the result to output list
         output.append(str(puzzle))
         puzzle[i][j], puzzle[i+1][j] = puzzle[i+1][j], puzzle[i][j]
 
     if j > 0:                                                      
-        puzzle[i][j], puzzle[i][j-1] = puzzle[i][j-1], puzzle[i][j]   #move left
+        puzzle[i][j], puzzle[i][j-1] = puzzle[i][j-1], puzzle[i][j]   # move left
+        # Add the result to output list
         output.append(str(puzzle))
         puzzle[i][j], puzzle[i][j-1] = puzzle[i][j-1], puzzle[i][j]
 
     if j < 3:                                   
-        puzzle[i][j], puzzle[i][j+1] = puzzle[i][j+1], puzzle[i][j]   #move right
+        puzzle[i][j], puzzle[i][j+1] = puzzle[i][j+1], puzzle[i][j]   # move right
+        # Add the result to output list
         output.append(str(puzzle))
         puzzle[i][j], puzzle[i][j+1] = puzzle[i][j+1], puzzle[i][j]
 
     return output
 
+# Heuristic function for A*
 def heuristic_manhattan(current_state):
     distance = 0
     puzzle = eval(current_state)
+    # Sum the total distance of each tile to its goal
     for i in range(4):
         for j in range(4):
             if puzzle[i][j] == 0: continue
             distance += abs(i - ((puzzle[i][j]-1)/4)) + abs(j -  ((puzzle[i][j]-1)%4));
     return distance
 
+# Function to print the report of moving step
 def print_move(previous, current):
     
     previous_puzzle = eval(previous)
@@ -61,6 +71,7 @@ def print_move(previous, current):
     elif(j < 3 and current_puzzle[i][j+1] == 0):
         return "RIGHT\t"
 
+# Function to print the report
 def report(path):
     for index, node in enumerate(path[1:]):
         if(index > 0):
@@ -68,27 +79,47 @@ def report(path):
         else:
             print("INITAIL\t", node)
 
+# A* algorithm
 def astar(start,end):
+    # Add the input test case to the list
     queue = [[heuristic_manhattan(start), start]]
     expanded = []
     expanded_nodes=0
+    
+    # Loop to expand the node until the list is became empty
     while queue:
         i = 0
+        # Looking for the best valule that calcuate from heuristic_manhattan and get its location
         for j in range(1, len(queue)):
             if queue[i][0] > queue[j][0]:
                 i = j
+        # Let the path equal to new value
         path = queue[i]
+        # Remove that from the queue
         queue = queue[:i] + queue[i+1:]
+        
+        # Check the node with the goal
         endnode = path[-1]
         if endnode == end:
             break
+        
+        # To save memory, if the node is expanded then skip
         if endnode in expanded: continue
+        
+        # Generate all posible moving step for the current sttate
         for k in moves(endnode):
+            # To save memory, if the node is expanded then skip
             if k in expanded: continue
+                
+            # Generate the new path for the tree for the next loop, and add it to the queue
             newpath = [path[0] + heuristic_manhattan(k) - heuristic_manhattan(endnode)] + path[1:] + [k]
             queue.append(newpath)
+            # Also add it to expanded list to protect for doing it again
             if endnode not in expanded: expanded.append(endnode)
-        expanded_nodes += 1
+                
+        expanded_nodes += 1 # count expanded nodes
+        
+        # Terminate the program when it has visited more than 1 million nodes
         if(expanded_nodes > 1000000):
             print("Solution not found!")
             sys.exit("The A* algorithm expands more than 1 million nodes.")
@@ -105,15 +136,17 @@ def main():
     goal = str([[1,2,3,4],[5,6,7,8], [9,10,11,12],[13,14,15,0]])
     
     start = datetime.datetime.now()
-    
     print("A*")
     astar(puzzle1,goal)
+    stop = datetime.datetime.now()
+    total = stop-start
+    print('\nExecution time:',total.total_seconds() * 1000, 'ms')
     
     print("\n--------------------------\n")
 
+    start = datetime.datetime.now()
     print("A*")
     astar(puzzle2,goal)
-
     stop = datetime.datetime.now()
     total = stop-start
     print('\nExecution time:',total.total_seconds() * 1000, 'ms')
